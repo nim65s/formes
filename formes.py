bd3e2cdc91d7229ee9f4740a5a105f942f001abd
Guilhem Saurel <guilhem.saurel@laas.fr>
2022-09-14 11:46:03 +0200

    ronds et quadrilatères

--- formes.py
+++ formes.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 class Forme:
     """Classe de base pour les autres formes.
 
@@ -15,12 +16,36 @@ class Forme:
 class Rond(Forme):
     a: float
 
+    def __init__(self, a: float):
+        self.a = a
+
     def __str__(self) -> str:
         return f"rond de rayon {self.a} de couleur {self.couleur}"
+
+
+class Quadrilatère(Forme):
+    a: float
+    b: float
+    c: float
+    d: float
+
+    def __init__(self, a: float, b: float, c: float, d: float):
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+
+    def __str__(self) -> str:
+        return (
+            f"quadrilatère de côtés {self.a}, {self.b}, {self.c} "
+            "et {self.d}  de couleur {self.couleur}"
+        )
 
 
 if __name__ == "__main__":
     f = Forme()
     print(f)
-    r = Rond()
+    r = Rond(10)
     print(r)
+    q = Quadrilatère(2, 2, 2, 2)
+    print(q)
