ab5bd19ee9fb89c0da0b83b7be2d07616cd5649a
Guilhem Saurel <guilhem.saurel@laas.fr>
2022-09-14 11:39:53 +0200

    add doctests

--- formes.py
+++ formes.py
@@ -12,6 +12,15 @@ class Forme:
         return f"forme indéfinie de couleur {self.couleur}"
 
 
+class Rond(Forme):
+    a: float
+
+    def __str__(self) -> str:
+        return f"rond de rayon {self.a} de couleur {self.couleur}"
+
+
 if __name__ == "__main__":
     f = Forme()
     print(f)
+    r = Rond()
+    print(r)
