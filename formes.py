f652d75a192f904e61dd1dcdff4ee70f6b22ac7e
Guilhem Saurel <guilhem.saurel@laas.fr>
2022-09-14 11:34:06 +0200

    implement formes.Forme

---
+++ formes.py
@@ -0,0 +1,7 @@
+class Forme:
+    """Classe de base pour les autres formes."""
+
+    color: str = "noire"
+
+    def __str__(self) -> str:
+        return f"forme indéfinie de couleur {self.color}"
