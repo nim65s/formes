5acdb4b81a64df8ea9c1f47c8cddbf5866f54794
Guilhem Saurel <guilhem.saurel@laas.fr>
2022-09-14 11:36:09 +0200

    couleur en français

--- formes.py
+++ formes.py
@@ -1,7 +1,7 @@
 class Forme:
     """Classe de base pour les autres formes."""
 
-    color: str = "noire"
+    couleur: str = "noire"
 
     def __str__(self) -> str:
-        return f"forme indéfinie de couleur {self.coler}"
+        return f"forme indéfinie de couleur {self.couleur}"
