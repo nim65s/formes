0a1acc57aeb8025bc8a692af3a61e20da6128d10
Guilhem Saurel <guilhem.saurel@laas.fr>
2022-09-14 11:38:43 +0200

    add doctests

--- formes.py
+++ formes.py
@@ -1,7 +1,17 @@
 class Forme:
-    """Classe de base pour les autres formes."""
+    """Classe de base pour les autres formes.
+
+    >>> f = Forme()
+    >>> print(f)
+    forme indéfinie de couleur noire
+    """
 
     couleur: str = "noire"
 
     def __str__(self) -> str:
         return f"forme indéfinie de couleur {self.couleur}"
+
+
+if __name__ == "__main__":
+    f = Forme()
+    print(f)
